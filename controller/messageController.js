c80a67968f4cd5958f880c6d5b4a0ad7b475d5f2
Sudipta077 <paulsudipta929@gmail.com>
2025-04-17 22:01:10 +0530

    finish

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,21 +1,21 @@
 import Message from '../models/messageModel.js'
 import User from '../models/userModel.js'
 import Chat from '../models/chatModel.js'
-import {GetObjectCommand, S3Client,PutObjectCommand} from '@aws-sdk/client-s3'
-import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+import { GetObjectCommand, S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import dotenv from 'dotenv';
 dotenv.config();
 const s3Client = new S3Client({
-    region:'ap-south-1',
-    credentials:{
-        accessKeyId:`${process.env.S3_ACCESS_KEY}`,
-        secretAccessKey:`${process.env.S3_ACCESS_SECRET}`
+    region: 'ap-south-1',
+    credentials: {
+        accessKeyId: `${process.env.S3_ACCESS_KEY}`,
+        secretAccessKey: `${process.env.S3_ACCESS_SECRET}`
     }
 })
 
 
 export const sendMessage = async ({ message, user }) => {
-    const { content, chatId,messageType } = message;
+    const { content, chatId, messageType } = message;
 
     if (!content || !chatId) {
         return { error: "Incomplete details" };
@@ -25,11 +25,11 @@ export const sendMessage = async ({ message, user }) => {
         sender: user.userId,
         content,
         chat: chatId,
-        messageType:messageType || 'text',
-        fileName:message?.fileName ,        // Optional: original file name
+        messageType: messageType || 'text',
+        fileName: message?.fileName,        // Optional: original file name
         mimeType: message?.mimeType,        // Optional: for frontend rendering logic
         fileSize: message?.fileSize,        // Optional: to show size on UI
-      
+
     };
     if (messageType !== 'text') {
         if (message.fileName) newMessage.fileName = message.fileName;
@@ -68,49 +68,61 @@ export const sendMessage = async ({ message, user }) => {
 };
 
 
-export const fetchMessage=async(req,res)=>{
-    try{
+export const fetchMessage = async (req, res) => {
+    try {
         // console.log(req.params.chatId);
-        const messages = await Message.find({chat:req.params.chatId}).populate('sender','name email picture').populate('chat');
+        const messages = await Message.find({ chat: req.params.chatId }).populate('sender', 'name email picture').populate('chat');
         res.status(200).json(messages);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        return res.status(500).json({message:err});
+        return res.status(500).json({ message: err });
     }
 }
 
 
 async function getObjectUrl(key) {
     const command = new GetObjectCommand({
-        Bucket:'chatfinity',
-        Key:key
+        Bucket: 'chatfinity',
+        Key: key
     });
-    const url = await getSignedUrl(s3Client,command);
+    const url = await getSignedUrl(s3Client, command);
     return url;
 }
 
-async function putObjectUrl(chatId,fileName,fileType) {
+async function putObjectUrl(chatId, fileName, fileType) {
     const command = new PutObjectCommand({
-        Bucket:'chatfinity',
-        Key:`${chatId}/${fileName}`,
-        ContentType:fileType
+        Bucket: 'chatfinity',
+        Key: `${chatId}/${fileName}`,
+        ContentType: fileType
     });
-    const url = await getSignedUrl(s3Client,command);
+    const url = await getSignedUrl(s3Client, command);
     return url;
 }
 
-export const sendFiles=async(req,res)=>{
-    const {fileName,fileSize,fileType} = req.body
-    console.log("hello from postFIle---------->");
-    const url = await putObjectUrl(req.params.chatId,fileName,fileType)
-    console.log("post url---->",url);
-    res.json({url:url});
+export const sendFiles = async (req, res) => {
+    const { fileSize, fileType } = req.body;
+    const fileName = Date.now();
+    try {
+        console.log("hello from postFIle---------->");
+        const url = await putObjectUrl(req.params.chatId, fileName, fileType)
+        console.log("post url---->", url);
+        res.json({ url: url,key:`${req.params.chatId}/${fileName}` });
+    }
+    catch (err) {
+        console.log("Error in sendFiles-->?", err);
+    }
 }
 
-export const getFiles=async(req,res)=>{
+export const getFiles = async (req, res) => {
     console.log("hello from getFIle---------->");
-    const url = await getObjectUrl('background.jpg')
-    console.log("getUrl--------->",url);
-    res.json({url:url});
+    try {
+        const key = req.query.key;
+        const url = await getObjectUrl(key);
+        console.log("getUrl--------->", key);
+        res.json({ url: url });
+    }
+    catch (err) {
+        console.log("Error at getFIle--->", err);
+    }
 }
